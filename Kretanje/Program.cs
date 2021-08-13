0c536afc6149a9e17ec7600b02626b0c376c36c1
Dragan Pepic <d.pepic@hotmail.com>
2021-08-13 15:33:06 +0200

    Add project files.

---
+++ Kretanje/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Kretanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int trenutaSoba = 1;
+            int brojSoba = 5;
+
+            do
+            {
+                Console.Clear();
+                int brojac = 1;
+                while(brojac <= brojSoba)
+                {
+                    if (trenutaSoba == brojac)
+                    {
+                        Console.Write("[*]");
+                    }else
+                    {
+                        Console.Write("[]");
+                    }
+                    if (brojac < brojSoba)
+                    {
+                        Console.Write("-");
+                    }
+                    brojac++;
+                }
+                Console.WriteLine();
+                Console.Write("Smer?");
+                string unos = Console.ReadKey().KeyChar.ToString();
+                if (unos == "d")
+                {
+                    trenutaSoba++;
+                }else if (unos == "a")
+                {
+                    trenutaSoba--;
+                }
+            } while (true);
+
+        }
+    }
+}
